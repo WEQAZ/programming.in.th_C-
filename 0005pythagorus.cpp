1266fc8228e784022993e241ee6533cd2ba59b4d
Ice <chanaicel@gmail.com>
2023-02-01 11:03:02 +0700

    add 0005 pythagorus

--- 0005pythagorus.cpp
+++ 0005pythagorus.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <cmath>
+
 
 using namespace std;
 
 int main()
 {
-    
+    double cathetus1,cathetus2;
+
+    cin >> cathetus1 >> cathetus2; 
+    double ans = (cathetus1*cathetus1) + (cathetus2*cathetus2);
+    printf("%lf",sqrt(ans));
+
     return 0;
 }
